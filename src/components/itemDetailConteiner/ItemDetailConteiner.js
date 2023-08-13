4e15bb9ad24de420e979a3e6acf8dfac423b549c
David Gomez <davidgomezarg@gmail.com>
2023-08-13 19:57:38 -0300

    Id de producto no encontrado en firebase nos arroja error 404

--- src/components/itemDetailConteiner/ItemDetailConteiner.js
+++ src/components/itemDetailConteiner/ItemDetailConteiner.js
@@ -1,48 +1,56 @@
-import { useState,useEffect } from "react";
-import { useParams,Navigate} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, Navigate } from "react-router-dom";
 import ItemDetail from "../itemDetail/ItemDetail.js"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import {getFirestore,doc,getDoc} from "firebase/firestore"
+import { getFirestore, doc, getDoc,collection } from "firebase/firestore"
 
 
-const ItemDetailConteiner = ()=>{
+const ItemDetailConteiner = () => {
 
-    const [producto,setProducto] = useState([]);
-    const {Id} = useParams();
-    const [error,setError]=useState(false);
+    const [producto, setProducto] = useState([]);
+    const { Id } = useParams();
+    const [error, setError] = useState(false);
 
-    useEffect(()=>{
-     const querydb= getFirestore();
-     const queryDoc= doc(querydb,"productos",Id) 
-     getDoc(queryDoc)
-     .then((res)=>{setProducto(({id:res.id, ...res.data()}))
-                    setError(false)
-                    })
-     .catch(setError(false))//Algo funciona mal. Se setea con false para que funcione. 
-    },[Id]);
+    useEffect(() => {
+        const querydb = getFirestore();
+        const queryDoc= doc(querydb,"productos",Id) 
 
+        getDoc(queryDoc).then((doc) => {
+            if (doc.exists()) {
+                setProducto(({id:doc.id, ...doc.data()}))
+                setError(false)
+            } else {
+                setError(true)
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
     
 
-    return(<>
-            {error ? <Navigate to="/*"/> 
-            :
-            <Container>
-                <Row className="justify-content-md-center">
-                    <ItemDetail
-                    id= {producto.id}
+
+    }, [Id]);
+
+
+return (<>
+    {error ? <Navigate to="/*" />
+        :
+        <Container>
+            <Row className="justify-content-md-center">
+                <ItemDetail
+                    id={producto.id}
                     image={producto.image}
                     name={producto.name}
                     price={producto.price}
                     category={producto.category}
                     description={producto.description}
                     stock={producto.stock}
-                    />
-                </Row>
-            </Container>           
-            }
-            </>      
-    )
+                />
+            </Row>
+        </Container>
+    }
+</>
+)
 }
 
 export default ItemDetailConteiner;
